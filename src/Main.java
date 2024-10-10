f52dd4cd4cf337328614d3eaac158daf6c8b04aa
Usuario <luis40105@edu.unipar.br>
2024-10-09 20:28:10 -0400

    PrimeiraAula,

--- src/Main.java
+++ src/Main.java
@@ -16,7 +16,7 @@ public class Main {
 
         int multiplicacaoFuncao = multiplicarValores (10,3);
         System.out.println("Valor Multiplicação: "+multiplicacaoFuncao);
-        //
+        //LUIS
 
         int divisaoFuncao = dividirValores (-10,2);
         System.out.println("Valor Divisão: "+divisaoFuncao);
